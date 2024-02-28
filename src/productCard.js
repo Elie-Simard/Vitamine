cc2e34a9380783817ab677ee7d05ceafc6494683
xynax739 <xynax739@gmail.com>
2024-02-28 09:00:28 -0500

    Commit final Alex

--- src/productCard.js
+++ src/productCard.js
@@ -5,6 +5,10 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const ProductCard = ({ productName, description, price, imageUrl }) => {
     
+    const handleIngredientsClick = () => {
+        alert("Afficher les ingrédients");
+    };
+    
     return (
         <Grid container justifyContent="center" style={{ marginTop: '20px' }}>
             <Grid item xs={12} md={6}>
@@ -20,15 +24,19 @@ const ProductCard = ({ productName, description, price, imageUrl }) => {
                         </Grid>
                         <Grid item>
                             <TextField id="outlined-number" label="Quantité" type="number" InputLabelProps={{ shrink: true }} variant="outlined" style={{margin:'5px'}}/>
-                            <Button variant="contained" color="primary" aria-label="view-details" style={{margin:'5px'}}>
-                            <IconButton color="primary" aria-label="add-to-cart">
-                                <AddShoppingCartIcon style={{color:'white'}}/>
-                            </IconButton>
+                            <Button variant="contained" color="primary" aria-label="add-to-cart" style={{margin:'5px'}}>
+                                <IconButton color="primary" aria-label="add-to-cart">
+                                    <AddShoppingCartIcon style={{color:'white'}}/>
+                                </IconButton>
                             </Button>
                             <Button variant="contained" color="primary" aria-label="view-details" style={{marginLeft:'5px', marginBottom:'5px', marginTop:'5px' }}>
-                            <IconButton color="primary" aria-label="view-details">
-                            <SearchIcon style={{color:'white'}}/>
-                            </IconButton>
+                                <IconButton color="primary" aria-label="view-details">
+                                    <SearchIcon style={{color:'white'}}/>
+                                </IconButton>
+                            </Button>
+                            {/* Bouton Ingrédients */}
+                            <Button variant="contained" color="primary" aria-label="view-ingredients" onClick={handleIngredientsClick} style={{marginLeft:'5px', marginBottom:'5px', marginTop:'5px' }}>
+                                Ingrédients
                             </Button>
                         </Grid>
                     </Grid>
